c8c45fce361e24a00b50dcb2c85e5fbfeece4514
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-07 16:19:45 -0800

    Got ready to roll a whole handful of dice.

--- die.py
+++ die.py
@@ -3,13 +3,15 @@
 
 from random import randint
 
+#Random number between 1 & 6
+
 def die():
 	
 	roll = randint(1, 6)
 	print("It's a die---->" + str(roll))
 
 
-die()
+
 
 # Custom sized die that takes a range
 
@@ -19,4 +21,17 @@ def custom_die(lower, upper):
 	print("It's a custom sized die---->" + str(roll))
 
 
-custom_die(1, 20)
+def main():
+	#Ask how many dice
+	total_rolls = input("How many dice you rollin'? ")
+	total_rolls = int(total_rolls)
+
+	#How big the die
+	max_num = input("How many sides per dice? ")
+	max_num = int(max_num)
+
+	#Roll that many dice
+	for something in range(1, total_rolls+1):
+		custom_die(1, max_num)
+
+main()

9cf514dae5614b43ca86bcaae760c884fa36dbbc
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-08 16:35:22 -0800

    A mud that passes function names around. Work in Progress

--- mud.py
+++ mud.py
@@ -14,24 +14,51 @@ Feel = "capricious and impatient"
 #Room "The barn floor."
 def barn_floor():
 	
-	lol = "" #l.ook o.r l.eap
+	lol = "" #l.ook o.r l.eap variable initialed
+	direction ="" #Direction of travel variable.
 	
 	print("The musty dirt under your goat hooves smells familiar,\nbut that's all you recognize.\nYou feel {}.".format(Feel))
 	
 	#Lets offer some options and get input
 	while lol == "":
+		
 		lol = input("Look or leap? ")
+		
 		if lol.lower() == "look":
-			print("Wow!")
-		elif lol.lower == "leap":
-			print("Ouch!")
-		elif lol.lower == "q":
+			print("Behind you is a staircase to an empty loft.\nRight is a pen filled with restless pigs.\nAhead is an open door.\nLeft is a warm stall.")
+			direction = input("Ahead, behind, left, or right? ")
+			print("You move {}.".format(direction))
+			exit()
+
+		elif lol.lower() == "leap":
+			print("You leap into a pig pen.")
+			return pigs
+
+		elif lol.lower() == "q":
 			exit()	
+		
 		else:
 			lol = ""
 			print("What?")
 	
 
+def pigs():
+	print("Pigs!")
+
+def ravine():
+	print("Ravine!")
+
+def play_hill():
+	print("play_hill")
+
+def pasture():
+	print("Pasture")
+
+def warm_stall():
+	print("A warm stall")
+
+def loft():
+	print("A loft")
 
 def intro():
 	print("\n\nYou are lucky. You don't use punctuation. You are a goat.\nYou know your 'q' to quit.")
@@ -55,6 +82,7 @@ def intro():
 
 def main():
 	intro()
-	barn_floor()
+	function_next = barn_floor()
+	function_next()
 
 main()

ffd7038c85f055f50d7b317e68bb0591291f2fa5
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-09 16:32:44 -0800

    Roughed out essential structure for our MUD

--- mud.py
+++ mud.py
@@ -1,42 +1,81 @@
 #Pseudocode for program loop
 #room we are in
 #opening room statement
-#cue to look or go
+#cue to look or leap without looking
 #look
-#go
-#room statement
-#loop
+#	cue for diection
+#	go requested direction
+#	update feeling and hitpoints
+#	room statement
+#leap
+#	go random direction
+#	update feeling and hitpoints
+#	room statement
+#loop back to look or leap
+
+from random import randint
+
 
 #Global variable for how the character feels
 Feel = "capricious and impatient"
-
+exit_message = "\nLater lazy goat!"
 
 #Room "The barn floor."
 def barn_floor():
 	
 	lol = "" #l.ook o.r l.eap variable initialed
 	direction ="" #Direction of travel variable.
-	
-	print("The musty dirt under your goat hooves smells familiar,\nbut that's all you recognize.\nYou feel {}.".format(Feel))
+	leap_direction = "right"
+	location = "barn"
+
+	print("The musty dirt under your goat hooves smells familiar,\nbut that's all you recognize in this old barn.\nYou feel {}.".format(Feel))
 	
 	#Lets offer some options and get input
 	while lol == "":
 		
 		lol = input("Look or leap? ")
 		
+		#LOOK#########################
 		if lol.lower() == "look":
-			print("Behind you is a staircase to an empty loft.\nRight is a pen filled with restless pigs.\nAhead is an open door.\nLeft is a warm stall.")
-			direction = input("Ahead, behind, left, or right? ")
-			print("You move {}.".format(direction))
-			exit()
+			print("Behind you is a staircase to an empty loft.\nRight is a pen filled with restless pigs.\nforwards is an open door.\nLeft is a warm stall.")
+			direction = input("Which way to move? forwards, backwards, left, or right? ")
+				
+			if direction.lower() == "forwards":
+				print("\nYou leap {}.".format(direction))
+				return pasture
+			elif direction.lower() == "backwards":
+				print("\nYou leap {}.".format(direction))
+				return loft
+			elif direction.lower() == "right":
+				print("\nYou leap {}.".format(direction))
+				return pigs
+			elif direction.lower() == "left":
+				print("\nYou leap {}.".format(direction))
+				return warm_stall
+			elif direction.lower() == "q":
+				print(exit_message)
+				exit()
+			elif direction.lower() == "p":
+				print("\nAhhh. This is my {}".format(location))
+				return barn_floor
+			else:
+				print("\nThat makes no sense. One to many headbutts maybe, eh?")
+				return barn_floor
+				
 
+
+
+		#LEAP########################
 		elif lol.lower() == "leap":
 			print("You leap into a pig pen.")
 			return pigs
 
+		#QUIT#######################
 		elif lol.lower() == "q":
+			print(exit_message)
 			exit()	
 		
+		#GIBBERISH HANDLING#########
 		else:
 			lol = ""
 			print("What?")
@@ -61,7 +100,7 @@ def loft():
 	print("A loft")
 
 def intro():
-	print("\n\nYou are lucky. You don't use punctuation. You are a goat.\nYou know your 'q' to quit.")
+	print("\n\nYou are lucky. You don't use punctuation, since you are a goat.\nYou know 'q' quits and 'p' pees. Mind your q's and p's!")
 	print( """	                         //  
 	                        ((   
 	                         \\__,

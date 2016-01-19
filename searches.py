aacf15ae3eeaaaa9bdd093cf316e4b0e64d8437d
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-19 14:46:08 -0800

    Binary search complete!

--- searches.py
+++ searches.py
@@ -6,11 +6,12 @@ def main():
 
 	# Make a list to look for the target value in.
 	unsorted_list = ["E", "Z", "L", "O", "B", "F"]
-	target_value = "B"
+	target_value = "F"
 
 	sorted_list, target_index = binary_search(unsorted_list, target_value)
 
 	#Print our solutions
+	print(sorted_list)
 	print("I found ", target_value, "It's at: ", target_index)
 	binary_search(unsorted_list, "B")
 
@@ -19,8 +20,42 @@ def main():
 def binary_search(the_list, target_value):
 	"""Implements binary search algorithm."""
 
+	#Sort the list
 	sorted_list = bubble_sort(the_list)
 	
+	# Search the target value
+	
+	# Find length of current segment, 
+	length = len(sorted_list)
+
+	# Initialize start and end vars
+	start = 0
+	end = length
+
+
+	# If len >= 1, look for target
+	while length >=1:
+
+		#Find mid point of the segment 
+		length = len(sorted_list[start:end])
+		mid = start + (length // 2)
+
+		# Determine if the middle value is greater or less than, or equal,
+		#If equal, we've found it return middle
+		if sorted_list[mid] == target_value:
+			return(sorted_list, mid)
+
+		#If greater, reduce the segment to the left half, repeat loop
+		elif sorted_list[mid] > target_value:
+			length = len(sorted_list[start:mid])
+			end = mid
+
+		#If less, reduce the segment to the right half, repeat loop
+		elif sorted_list[mid] < target_value:
+			start = mid + 1
+
+		# Reeval length before loop runs
+		length = len(sorted_list[start:end])
 
 	#IF we can't find the index, return  -1
 	return (sorted_list, -1)

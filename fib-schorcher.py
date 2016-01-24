cd397db35fd2cd4a9a8603262f3da7eea9227429
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-23 18:34:34 -0800

    Fun fib prog improved.

--- fib-schorcher.py
+++ fib-schorcher.py
@@ -1,6 +1,19 @@
-import functtools
-@lru_cache(max_size = none)
+from functools import lru_cache
+
+
+@lru_cache(maxsize = None)
 def fib(num):
+	print(num)
 	if num == 0 or num == 1:
 		return num
 	else: return fib(num-1)+fib(num-2)
+
+def main():
+
+	x = int(input("Give me an integer to calculate the nth Fibbonacci:"))
+	i = 0
+	while i < x:
+		print(fib(int(i)), end = " ")
+		i += 1
+
+main()

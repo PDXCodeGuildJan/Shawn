95c6941db22dfdfc0412b1602290dd80bf574fcf
shawnwaldow <shawnwaldow@hotmail.com>
2016-01-23 19:10:01 -0800

    Comments extended in TOHP.

--- Hanoi.py
+++ Hanoi.py
@@ -18,56 +18,77 @@ Track number of moves and compare it to the begining number of disks.
 
 """
 
-
+# Remove the last value of a list and return it
 def pop_top(list1):
 	length = len(list1)
 	val = list1.pop(length-1)
 	return val
 
+
+
+# Extend the list from the end with value
 def push_top(list1, val):
 	length = len(list1)
 	list1.insert(length,  val)
 
+
+
+
+# Move the "top" value of a tower to the destination tower IFF the plate size rules of towers of hanoi aren't violated.
 def tower_to_tower(source_tower, destination_tower):
 	
 	temp = pop_top(source_tower)
 	
+	# Always ok to move into an empty list
 	if len(destination_tower) < 1:
 		push_top(destination_tower, temp)
+
+	# If the list isn't empty make sure the top plate isn't small than the one we are putting there.
 	elif temp < destination_tower[-1]:
 		push_top(destination_tower, temp)
+
 	else:
 		print("Can't stack a big plate atop a smaller plate.")
 		exit()
 
+
+# Lengthy function to print the towers in order no matter how they are passed into this print function.
+# Takes three lists plus a dictionary that lets us assign a name, A, B, C, according to actual adress in memory.
+# Makes use of standard Python 3 function id() which returns the memory location we want.
 def display_towers(tower1,tower2,tower3, tower_dict):
 	print("\n")
 	
+	# Arguments in A, B, C print A, B, C
 	if tower_dict[hex(id(tower1))] == "A" and tower_dict[hex(id(tower2))] == "B" and tower_dict[hex(id(tower3))] =="C":
 		print(tower_dict[hex(id(tower1))], tower1)
 		print(tower_dict[hex(id(tower2))], tower2)
 		print(tower_dict[hex(id(tower3))], tower3)
 
+	# Arguments in B, A, C print A, B, C
 	elif tower_dict[hex(id(tower1))] == "B" and tower_dict[hex(id(tower2))] == "A" and tower_dict[hex(id(tower3))] =="C":
 		print(tower_dict[hex(id(tower2))], tower2)
 		print(tower_dict[hex(id(tower1))], tower1)
 		print(tower_dict[hex(id(tower3))], tower3)
 
+	# Arguments in C, A, B print A, B, C
 	elif tower_dict[hex(id(tower1))] == "C" and tower_dict[hex(id(tower2))] == "A" and tower_dict[hex(id(tower3))] =="B":
 		print(tower_dict[hex(id(tower2))], tower2)
 		print(tower_dict[hex(id(tower3))], tower3)
 		print(tower_dict[hex(id(tower1))], tower1)
 
+	# Arguments in A, C, B print A, B, C
 	elif tower_dict[hex(id(tower1))] == "A" and tower_dict[hex(id(tower2))] == "C" and tower_dict[hex(id(tower3))] =="B":
 		print(tower_dict[hex(id(tower1))], tower1)
 		print(tower_dict[hex(id(tower3))], tower3)
 		print(tower_dict[hex(id(tower2))], tower2)
 
+	# Arguments in B, C, A print A, B, C
 	elif tower_dict[hex(id(tower1))] == "B" and tower_dict[hex(id(tower2))] == "C" and tower_dict[hex(id(tower3))] =="A":
 		print(tower_dict[hex(id(tower3))], tower3)
 		print(tower_dict[hex(id(tower1))], tower1)
 		print(tower_dict[hex(id(tower2))], tower2)
 
+	# Arguments in C, B, A print A, B, C
 	elif tower_dict[hex(id(tower1))] == "C" and tower_dict[hex(id(tower2))] == "B" and tower_dict[hex(id(tower3))] =="A":
 		print(tower_dict[hex(id(tower3))], tower3)
 		print(tower_dict[hex(id(tower2))], tower2)
@@ -81,7 +102,7 @@ def display_towers(tower1,tower2,tower3, tower_dict):
 
 
 
-
+#Recursive solution to the towers of hanoi puzzle. Relies on divide and conquer plus tricky passing list names around. 
 def hanoi(n, source_tower, dest_tower, spare_tower, tower_dict):
 	"""
 	1. Move all disks except the largest to tower b
@@ -93,21 +114,23 @@ def hanoi(n, source_tower, dest_tower, spare_tower, tower_dict):
 	# Move largest disk to destination tower
 	if n == 0:
 		
+		# Base Case
 		tower_to_tower(source_tower, dest_tower)
 		display_towers(source_tower, spare_tower, dest_tower, tower_dict)
 
 	else:
 
+		#  Move all disks except the largest to the spare tower
 		hanoi(n-1, source_tower, spare_tower, dest_tower, tower_dict)
 		display_towers(source_tower, spare_tower, dest_tower, tower_dict)
 
 
-		
+		# Move the largest disk to the destination tower
 		tower_to_tower(source_tower, dest_tower)
 		display_towers(source_tower, spare_tower, dest_tower, tower_dict)
 
 
-		
+		# Move all the remaining disks to the destination tower.		
 		hanoi(n-1, spare_tower, dest_tower, source_tower, tower_dict)
 		display_towers(source_tower, spare_tower, dest_tower, tower_dict)
 
@@ -119,36 +142,40 @@ def main():
 	tower2 = []
 	tower3 = []
 
-#Hard coded solution to a 3 disk tower moving from tower1 to end destination tower2.
+
+	tower_dict = {hex(id(tower1)): "A", hex(id(tower2)): "B", hex(id(tower3)): "C"}
+
+
+	#Hard coded solution to a 3 disk tower moving from tower1 to end destination tower2.
 	#display_towers(tower1,tower2,tower3)
 	
 	tower_to_tower(tower1, tower3)
 	
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 	
 	tower_to_tower(tower1, tower2)
 	
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 	
 	tower_to_tower(tower3, tower2)
 	
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 
 	tower_to_tower(tower1, tower3)
 
-	#display_towers(tower1,tower2,tower3)
-	
+	display_towers(tower1, tower2, tower3, tower_dict)
+
 	tower_to_tower(tower2, tower1)
 
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 
 	tower_to_tower(tower2, tower3)
 
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 
 	tower_to_tower(tower1, tower3)
 
-	#display_towers(tower1,tower2,tower3)
+	display_towers(tower1, tower2, tower3, tower_dict)
 
 	print("\n\n@@@@@@@@@@@@RESET AND RECURSE@@@@@@@@@@@@@@@@@\n\n")
 	
@@ -158,9 +185,9 @@ def main():
 	tower2 = []
 	tower3 = []
 
+	# Define a dictionary so we can track the three towers by memory location as they are passed around and aliased extensively in the recursive solution.
 	tower_dict = {hex(id(tower1)): "A", hex(id(tower2)): "B", hex(id(tower3)): "C"}
 
-	display_towers(tower1, tower2, tower3, tower_dict)
 
 	hanoi(int(len(tower1))-1, tower1, tower2, tower3, tower_dict)
 

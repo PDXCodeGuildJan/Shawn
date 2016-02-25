0ec01e8fbd3e641dce42c2273abf375585682e37
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-25 11:16:00 -0800

    cleaning up

--- casino/casino.js
+++ casino/casino.js
@@ -3,7 +3,12 @@ var  num = 6;
 
 var inputField = document.getElementById("num-loops");
 var counter = document.getElementById("counter");
-var faces = ["<img src=\"./dice/1.png\" alt=\"1\" />","<img src=\"./dice/2.png\" alt=\"2\" />","<img src=\"./dice/3.png\" alt=\"3\" />","<img src=\"./dice/4.png\" alt=\"4\" />","<img src=\"./dice/5.png\" alt=\"5\" />","<img src=\"./dice/6.png\" alt=\"6\" />"]
+var faces = ["<img src=\"./dice/1.png\" alt=\"1\" />",
+			"<img src=\"./dice/2.png\" alt=\"2\" />",
+			"<img src=\"./dice/3.png\" alt=\"3\" />",
+			"<img src=\"./dice/4.png\" alt=\"4\" />",
+			"<img src=\"./dice/5.png\" alt=\"5\" />",
+			"<img src=\"./dice/6.png\" alt=\"6\" />"]
 
 
 //add click listener

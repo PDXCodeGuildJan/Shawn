cdfce3f16ed765a590b68194702beb29f12d88bf
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-25 11:16:53 -0800

    Need to fix non fatal error with node picked up from last checkbox.

--- price_list/pricing/js/pricing.js
+++ price_list/pricing/js/pricing.js
@@ -17,30 +17,27 @@ removeStockButton.onclick = removeStock;
 function addStock() {
 	console.log("good");
 	//NOT ALLOWED TO USE QuerySelectorAll()
-	
+	//Get all the input tags
+
 	var input = document.getElementsByTagName("input");
-	
-	//find allthe selected things
 
-	 var checkedboxes = [];
+	var checkedboxes = [];
 
-    //loop through all elements and extract the info.
+    //loop through for the number of input tags grabbed.
     for (var i=0; i<input.length; i++){
        
-        // make an if statement that specifies which input we want to use
+        // Grab only the checked boxes
         if (input[i].checked === true){
-            // start a loop that identifies whether the box is checked or not
-                // Make a list for the checked boxes to go into
+
             checkedboxes.push(input[i]);
 
+        //traverse the DOM
         var status = input[i].parentNode.nextSibling.nextSibling.nextSibling;
         status.textContent = "yes";
         status.className = "true";
 
             console.log("This should be checked boxes", checkedboxes)
 
-            // change the inStock value of the selected things   
-            //for (var 1=0; i<checked.length; i++)
             };
     };
 };

dc48eca1972de75287fe7ec1297c990f8027a6f3
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-24 17:29:55 -0800

    add stock remove stock 99%

--- price_list/pricing/js/pricing.js
+++ price_list/pricing/js/pricing.js
@@ -7,24 +7,61 @@ addItemButton.onclick = addItem;
 var addStockButton = document.getElementById("add-stock");
 addStockButton.onclick = addStock;
 
+var removeStockButton = document.getElementById("remove-stock");
+removeStockButton.onclick = removeStock;
+
 /*	Add the item in the text fields to the inventory
 *	list which is in the table body (id="inventory")
 */
 
 function addStock() {
-	console.log("good")
+	console.log("good");
 	//NOT ALLOWED TO USE QuerySelectorAll()
-
+	
+	var input = document.getElementsByTagName("input");
+	
 	//find allthe selected things
 
-	//change their inStock value of the selected things
+	 var checkedboxes = [];
 
-	//update the display?
-}
+    //loop through all elements and extract the info.
+    for (var i=0; i<input.length; i++){
+       
+        // make an if statement that specifies which input we want to use
+        if (input[i].checked === true){
+            // start a loop that identifies whether the box is checked or not
+                // Make a list for the checked boxes to go into
+            checkedboxes.push(input[i]);
+
+        var status = input[i].parentNode.nextSibling.nextSibling.nextSibling;
+        status.textContent = "yes";
+        status.className = "true";
+
+            console.log("This should be checked boxes", checkedboxes)
+
+            // change the inStock value of the selected things   
+            //for (var 1=0; i<checked.length; i++)
+            };
+    };
+};
+
+
 
 function removeStock() {
 	// USE querySelectorAll()
-}
+	var allTRsInTarnation = document.querySelectorAll('tbody>tr>td>input:checked');
+	
+	//loop through the array we made.
+	for (var i=0; allTRsInTarnation.length > i; i++){
+		var status = allTRsInTarnation[i].parentNode.parentNode.children[3];
+		status.textContent = "No";
+		status.className="false";
+
+		}
+
+	console.log(allTRsInTarnation);
+
+};
 
 function addItem() {
 	var materialName = document.getElementById("name").value;
@@ -35,11 +72,13 @@ function addItem() {
 	var yesno = "no"
 	if (checked) 
 		{yesno = "yes"; 
-		truefalse = "true";} 
+		truefalse = "true";
+		} 
 	else 
 		{yesno = "no"; 
-		truefalse = "false";};
-	var newRow = "<tr><td><input type='checkbox' id='inasec' /></td><td>" + materialName + "</td> <td>"+ price +"</td><td class='"+truefalse+"'>" + yesno + "</td></tr>";
+		truefalse = "false";
+	};
+	var newRow = "<tr><td><input type='checkbox' id='this-item' /></td><td>" + materialName + "</td><td>"+ price +"</td><td class='"+truefalse+"'>" + yesno + "</td></tr>";
 	inventory.innerHTML += newRow
 
 };

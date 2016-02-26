c5541b361b866c01bdbc3cdbe158eb6813605897
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-26 14:58:02 -0800

    Oppsy infinite loop?

--- javapic/javapic/js/join_old.js
+++ javapic/javapic/js/join_old.js
@@ -1,10 +1,19 @@
 // Javapic Inc.
 // Join (Let's scrub)
 
+function submit(){
+	var name = document.getElementsByName("name");
+	console.log("Hello 2");
+	console.log(name/*.nextSibling.textContent*/);
+}
 // DONT FORGET TO DISABLE BROWSER SCRUBBING
 var grabFormKillDefaultScrub = document.getElementById('signup');
 var noValidate = document.createAttribute("novalidate");
 grabFormKillDefaultScrub.setAttributeNode(noValidate);
 
+
+// Catch the submit click
+var submitButton = document.getElementById("submit");
+submitButton.onclick = submit;
 
 console.log("Hello");
--- javapic/javapic/js/jumbotron.js
+++ javapic/javapic/js/jumbotron.js
@@ -6,27 +6,21 @@
 //Build up list of .jpg file names. Right now we have
 //60 images pdxcg_01.jpg thru pdxcg_60.jpg
 
+
 var picnames = [null];
 
 function setupStuff(picnames){
 
-for (var i = 1; i <= 60; i++) {
-	if (i <10){
-		picnames[i-1] = "pdxcg_0" + i + ".jpg";	
+	for (var i = 1; i <= 60; i++) {
+		if (i <10){
+			picnames[i-1] = "../images/pdxcg_0" + i + ".jpg";	
+		}
+		else {
+		picnames[i-1] = "../images/pdxcg_" + i + ".jpg";
+		}
 	}
-	else {
-	picnames[i-1] = "pdxcg_" + i + ".jpg";
-	}
-	//return picnames;
 
-
-//picnames = setupStuff();
-/*console.log(picnames);
-*/
-
-//setInterval(console.log("hello?"), 1000);
-
-return picnames;
+	return picnames;
 }
 
 
@@ -35,5 +29,17 @@ return picnames;
 
 //setInterval(setupStuff, 1000)
 picnames = setupStuff(picnames);
+i = 0
 
-console.log(picnames)
+function hello(){console.log("HEllo");};
+
+while (true){
+	document.getElementById("jumbotron").style.backgroundImage = "url('"+ picnames[0] +"')";
+
+	
+
+	i++;
+
+	window.setTimeout(hello, 10000);
+
+}

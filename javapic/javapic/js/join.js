e2f81346b4f5cf9c3185113afa701b5a70921de1
shawnwaldow <shawnwaldow@hotmail.com>
2016-02-29 12:01:27 -0800

    Lunch! Working on submit() research, trial, error.

--- javapic/javapic/js/join.js
+++ javapic/javapic/js/join.js
@@ -1,5 +1,6 @@
 // Javapic Inc.
-// Join (Let's scrub)
+// Author Shawn Waldow
+// Join (Let's scrub a basic form.)
 
 // DONT FORGET TO DISABLE BROWSER SCRUBBING
 var grabFormKillDefaultScrub = document.getElementById('signup');
@@ -7,4 +8,37 @@ var noValidate = document.createAttribute("novalidate");
 grabFormKillDefaultScrub.setAttributeNode(noValidate);
 
 
+
+
+//Get a handle on the form in the DOM
+var theForm = document.getElementById("signup");
+
+//Select a single element for name
+var theName = document.getElementsByName("name")[0];
+theName.addEventListener('blur', checkName, false);
+
+function checkName(){
+
+	//See if we are getting everthing we expect. Later scrub.
+	if (this.value.length < 2){
+		console.log("not long enough")
+	}
+	else{
+		console.log("else")
+	}
+}
+
+
+
+//Get a handle on the submit button
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", submitForm, false);
+
+function submitForm(){
+
+	console.log(theName[0].value)
+	
+}
+
+//console.log(theName.value);
 console.log("Hello");
--- javapic/javapic/js/jumbotron.js
+++ javapic/javapic/js/jumbotron.js
@@ -1,3 +1,4 @@
+//Javapic Inc.
 //Author Shawn Waldow Feb 2016
 //
 //javascript to make a title page's image cycle every 10 seconds.
@@ -11,14 +12,18 @@ var numPics = 60;
 
 var picnames = [null];
 
+
 function setupStuff(picnames, numPics){
 
+//Make a file name for numPics total files.
 	for (var i = 1; i < numPics; i++) {
 		
 		if (i <10){
 			picnames[i-1] = "./images/pdxcg_0" + i + ".jpg";	
 		}
 
+		//CIRCLE BACK AFTER MVP: There is still an extra null element in this 
+		//file list causing a non-fatal warning.	
 		else {
 		picnames[i-1] = "./images/pdxcg_" + i + ".jpg";
 		}

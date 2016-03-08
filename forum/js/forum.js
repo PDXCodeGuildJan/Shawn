520611fbb27f881b66ef2bcb7d6223c4f4e5d468
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-08 11:18:43 -0800

    Clean up.

--- forum/js/forum.js
+++ forum/js/forum.js
@@ -2,5 +2,9 @@ console.log("JAVASCRIPT HAS LOADED")
 
 var url = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script'
 
-$.ajax({type: 'GET', url: url, dataType: "JSONP", success: function(data){console.log(data)}
+$.ajax({
+	type: 'GET', 
+	url: url, 
+	dataType: "JSONP", 
+	success: function(data){console.log(data)}
 })

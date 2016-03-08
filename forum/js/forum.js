16fdab128d84972c60ff329dd713e677bf53be63
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-07 16:07:53 -0800

    Dang! Finally good AJAX!

--- forum/js/forum.js
+++ forum/js/forum.js
@@ -1,34 +1,6 @@
-// XML Http request object request
-var xhr = new XMLHttpRequest();
-var thePostsJSONP;
+console.log("JAVASCRIPT HAS LOADED")
 
-// When response loads
-xhr.onload = function() {
-	// The following conditional check will not work locally.
-	if(xhr.status === 200) {
-		thePostsJSONP = xhr.responseText;
-	}
-};
+var url = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script'
 
-
-
-xhr.open('GET', 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script', true);
-xhr.send(null);
-
-console.log(thePostsJSONP);
-
-//Load as JSONP
-//JSON.parse()
-
-/*function ajaxGet (){
-	// The following conditional check will not work locally.
-	if(xhr.status === 200) {
-		document.getElementById('gsx$post').innerHTML = xhr.responseText;
-	}
-};
-
-$.get('https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script', ajaxGet ,XML)
-
-
-
-*/
+$.ajax({type: 'GET', url: url, dataType: "JSONP", success: function(data){console.log(data)}
+})

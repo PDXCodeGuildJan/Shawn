b6c2b9bae50884463eeb540c6d0f7cd1fb963971
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-11 13:36:13 -0800

    Progress on portfolio and pollsite

--- django/pollproject/poll_site/models.py
+++ django/pollproject/poll_site/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	"""Defines the question class and model."""
+
+	# Define the prop of the question Model
+	# that are stored in the DB.
+	question_text = models.CharField(max_length=200)
+	
+	pub_date = models.DateTimeField('date published')
+
+class choice(models.Model):
+	"""Defines the choice class and model. Ids are automagic."""
+
+	choice_text = models.CharField(max_length=150)
+	votes = models.PositiveSmallIntegerField(default=0)
+	#Establish th one to many relationship from question to choice.
+	#Picks up the auoto generated id in question automagically.
+	question_id = models.ForeignKey(Question)
+	# now after this we ran python3 manage.py makemigrations
+	#in the virtual environment. then we ran
+	#$python3 manage.py migrate
+
+
---
+++ django/portfolio/portfolio/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+#We added this to handle the HttpResponse in this particular manner.
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def zen_garden_render(request):
+	return render(request, 'zen_garden/zen_mockup.html')
+
+def javapic_render(request):
+	return render(request, 'javapic/index.html')

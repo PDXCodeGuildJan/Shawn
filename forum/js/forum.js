73ad7776b203585b2b0826587de5ed30d20446b1
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-13 16:20:44 -0700

    Almost to MVP. Weird timeing error still needs worked out. Must colorize css and give lucas credit.

--- forum/js/forum.js
+++ forum/js/forum.js
@@ -5,21 +5,31 @@ grabFormKillDefaultScrub.setAttributeNode(noValidate);
 
 console.log("JAVASCRIPT HAS LOADED")
 
-var url = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script';
-
+var getUrl = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script';
+var postUrl = "https://docs.google.com/forms/d/1W1jO8DwInFuzlabeYqEp6hUUUqDbR8gESs1Pk6qozOc/formResponse";
+var newPostsTitle = document.getElementById('title');
 var theNewPost = document.getElementsByName('textarea');
-
 var postsDiv = document.getElementById('main-posts');
+
 
 $.ajax({
 	type: 'GET', 
-	url: url, 
+	url: getUrl, 
 	dataType: "JSONP", 
-	success: getPostsOnPageLoad,
+	success: getPosts,
 });
 
-function getPostsOnPageLoad(data){
+function removePosts(){
+	while (postsDiv.hasChildNodes()){
+		var child = postsDiv.firstChild;
+		var oldChild = postsDiv.removeChild(child);
+		}
+}
+
+function getPosts(data){
 	
+	removePosts();
+
 	for (i=0; i<data.feed.entry.length; i++){
 	
 	var h2 = document.createElement('h2');
@@ -47,10 +57,34 @@ function submitPost(event) {
 	event.preventDefault();
 
 
+	$.ajax({
+		url: postUrl,
+		//These entry objects are mysterious google spreadsheet stuff.
+		data: {"entry.1639809944" : newPostsTitle.value, "entry.1402022725" : theNewPost[0].value},
+		type: "POST",
+		dataType: "xml",
+		success: function(data){console.log(data);},
+		statusCode: {
+			0: function() {
+		
+				console.log("0 error good.");	
+				
+				$.ajax({
+					type: 'GET', 
+					url: getUrl, 
+					dataType: "JSONP", 
+					success: getPosts,
+				});
+
+			},
+
+			200: function() {
+			//make some error handling
+			}
+		}
+	});
+
 
 	console.log("posted pal");
 	console.log(theNewPost[0].value);
-	console.log("title ",data);
-	console.log("post ",data);
-	console.log("time",data);
 }

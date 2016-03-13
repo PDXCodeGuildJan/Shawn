1217553a4f3796cb152118a5dc868b2f7853e812
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-13 13:16:55 -0700

    Whew fixed really bad css and fixed buttons to a text area and submit button.

--- forum/js/forum.js
+++ forum/js/forum.js
@@ -1,10 +1,30 @@
+// DONT FORGET TO DISABLE BROWSER SCRUBBING
+var grabFormKillDefaultScrub = document.getElementById('signup');
+var noValidate = document.createAttribute("novalidate");
+grabFormKillDefaultScrub.setAttributeNode(noValidate);
+
 console.log("JAVASCRIPT HAS LOADED")
 
-var url = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script'
+var url = 'https://spreadsheets.google.com/feeds/list/1NlIJLGd32t38kt6mJgc64SFpDM_ltq7nfzaRjV1wpLI/default/public/values?alt=json-in-script';
+
+var theNewPost = document.getElementsByName('textarea');
 
 $.ajax({
 	type: 'GET', 
 	url: url, 
 	dataType: "JSONP", 
-	success: function(data){console.log(data)}
+	success: function(data){console.log(data);},
 })
+
+
+document.getElementById('submit').addEventListener('click', submitPost);
+
+function submitPost(event) {
+
+	//Mysterious bit necesarry for custom form submission.
+	event.preventDefault();
+
+	console.log("posted pal");
+	console.log(theNewPost[0].value);
+
+}

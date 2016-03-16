d2d25626e00475a3b92ec25373ca1a21e686fe70
shawnwaldow <shawnwaldow@hotmail.com>
2016-03-16 10:30:32 -0700

    removed ve for pollsite.

--- django/portfolio/VM4Portfolio/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/shawnwaldow/Documents/CodeSchool/Week1/django/portfolio/VM4Portfolio/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
